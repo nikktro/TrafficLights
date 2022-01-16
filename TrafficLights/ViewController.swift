ac85fd9a3d24840a3912f17944be98faee4ec891
Nikolay Trofimov <nikktro@gmail.com>
2022-01-16 03:29:40 +0300

    add app logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
+    private var currentState = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        changeAlpha(to: 0.3)
+        
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+        actionButton.layer.cornerRadius = actionButton.frame.height / 4
     }
-
-
+    
+    
+    @IBAction func actionButtonPressed() {
+        switch currentState {
+        case 1:
+            changeAlpha(to: 0.3)
+            redLight.alpha = 1
+            actionButton.setTitle("NEXT", for: .normal)
+            currentState += 1
+        case 2:
+            changeAlpha(to: 0.3)
+            yellowLight.alpha = 1
+            currentState += 1
+        case 3:
+            changeAlpha(to: 0.3)
+            greenLight.alpha = 1
+            currentState += 1
+        default:
+            changeAlpha(to: 0.3)
+            actionButton.setTitle("START", for: .normal)
+            currentState = 1
+        }
+    }
+    
+    
+    private func changeAlpha(to alpha: CGFloat) {
+        redLight.alpha = alpha
+        yellowLight.alpha = alpha
+        greenLight.alpha = alpha
+    }
+    
 }
 
